c041e63009ca616f21a64bdeaa6e062b5dce2913
ybadr99 <yousefbadr909@gmail.com>
2023-12-01 02:07:58 +0200

    fix linters

--- app/controllers/random_messages_controller.rb
+++ app/controllers/random_messages_controller.rb
@@ -1,6 +1,6 @@
 class RandomMessagesController < ApplicationController
-      def random_greeting
-            message =  RandomMessage.order("RANDOM()").first
-            render json: { greeting: message.content }
-      end
+  def random_greeting
+    message = RandomMessage.order('RANDOM()').first
+    render json: { greeting: message.content }
+  end
 end
